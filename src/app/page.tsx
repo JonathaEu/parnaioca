d8e75e173540a6afa768ca1ef1bd5efedced663b
Victor.Loperz <euvictor_h@hotmail.com>
2023-08-09 08:06:39 -0300

    primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,9 @@ import LogoTipo from '../../public/assets/logo.png'
 export default function Home() {
   return (
     <div>
-    <header className="bg-[url('../../public/assets/header.jpg')] w-full bg-cover p-4 flex items-center justify-between">
-        <div className="w-32 h-auto">
-          <a href="/"><img src={LogoTipo.src} alt="Logotipo"/></a>
-        </div>
-
-        <nav className="space-x-4 items-center text-xs">
-          <a href="./app/funcionario/login" className="text-black">Login</a> ·
-          <a href="#" className="text-black">Cadastrar clientes</a> ·
-          <a href="#" className="text-black">Sobre</a> ·
-        </nav>
-    </header>
+      <h1>
+        Início
+      </h1>
     </div>    
   )
 }
