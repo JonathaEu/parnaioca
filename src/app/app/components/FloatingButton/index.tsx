6ab4e0f9d9e8b2ecb6334262db95297bb969cc6e
Victor.Loperz <euvictor_h@hotmail.com>
2023-08-11 12:42:21 -0300

    cria floating button

---
+++ src/app/app/components/FloatingButton/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Whatsapp from '../../../../../public/assets/whatsapp.gif';
+
+function FloatingButton({ children }: any) {
+    return (
+        <>
+            <div className="right-8 py-10 pt-[30rem] w-20 h-auto fixed">
+                <a href="#"><img src={Whatsapp.src} alt="Whatsapp"/></a>
+            </div>
+            {children}
+        </>
+    )
+}
+
+export default FloatingButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Sidebar from "./app/components/Sidebar";
 import Footer from './../app/app/components/Footer';
+import FloatingButton from "./app/components/FloatingButton";
+
 
 export default function Home(){
   return(
     <>
     <div>
-    <Sidebar/>
+    <Sidebar>
+    <FloatingButton/>
+    </Sidebar>
     <Footer/>
     </div>
     </>
