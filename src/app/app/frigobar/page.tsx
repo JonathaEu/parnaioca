672951f911bb10db45c6c5dca644a8222cf0ae13
Jonatha Cunha - TI <Jonatha.cunha@MASAN.COM.BR>
2023-09-14 14:57:54 -0300

    real page frigobar

--- src/app/app/frigobar/page.tsx
+++ src/app/app/frigobar/page.tsx
@@ -2,14 +2,13 @@
 import React from 'react'
 import Cadastro from '../../../../public/assets/cadastro.png'
 import LogoTipo from '../../../../public/assets/logo.png'
+import Sidebar from '@/app/app/components/Sidebar'
 import Footer from '@/app/app/components/Footer'
 import { useState, useEffect } from 'react';
 import api from '@/services/api'
 import FormElements from '@/app/modals/RegisterFrigobar'
-import SideBarFuncionario from '../components/SideBarFuncionario'
 import EditFrigobarModal from '@/app/modals/editFrigobar'
 import ItensIntoFrigobar from '@/app/modals/ItensIntoFrigobar'
-
 
 type Inputs = {
     itens_id: number
@@ -43,33 +42,36 @@ export default function Frigobar() {
 
     return (
         <>
-        <SideBarFuncionario>
-                       <FormElements />
-                        <section>
-                            <div>
-                                <table className="rounded-lg">
-                                    <thead className="text-xs uppercase dark:bg-[#2a2c45] dark:text-white">
-                                        <tr className="">
-                                            <th scope="col" className="px-16 p-2">
-                                                Número do Frigobar:
-                                            </th>
-                                            <th scope="col" className="px-20 p-2">
-                                                Quarto:
-                                            </th>
-                                            <th scope="col" className="px-20 p-2">
-                                                Situação:
-                                            </th>
-                                            <th scope="col" className="px-2 p-2">
-                                            </th>
-                                        </tr>
-                                    </thead>
+            <Sidebar>
+                <div className="bg-[url('/assets/ilha.jpg')] bg-cover w-full h-screen">
+                    <section className="flex flex-wrap content-between ">
+                        <FormElements getFrigobar={getFrigobar} />
+                        <div className='w-full px-10'>
+                            <table className="w-full text-sm text-left text-white dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 w-full">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            Frigobar número:
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Está no quarto:
+                                        </th>
+                                        <th scope="col" className="px-4 py-3">
+                                            Situação:
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                        </th>
+                                    </tr>
+                                </thead>
+
+
                                 <tbody className=''>
                                     {frigobar.map((frigobar: any, index) => {
                                         let status = frigobar.ativo ? "Ativo" : "Inativo";
                                         const classNameGreen = "bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400"
                                         const classNameRed = "bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400"
                                         let statusStyle = frigobar.ativo ? classNameGreen : classNameRed;
-                                       
+
                                         return (
                                             <>
                                                 <tr key={frigobar.id} className=' bg-cyan-800 border-b dark:bg-gray-900 dark:text-white'>
@@ -97,10 +99,10 @@ export default function Frigobar() {
                                 </tbody>
                             </table>
                         </div>
-                        </section>
-                    </div>
+
+                    </section>
                 </div>
-            </SideBarFuncionario>
+            </Sidebar>
         </>
     )
 }
