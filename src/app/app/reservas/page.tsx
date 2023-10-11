7f91bab73962d4a7be6f5d4eb5cd8995c789db8b
Jonatha Cunha - TI <Jonatha.cunha@MASAN.COM.BR>
2023-10-11 13:32:26 -0300

    visualizar reserva funcionando

--- src/app/app/reservas/page.tsx
+++ src/app/app/reservas/page.tsx
@@ -9,6 +9,7 @@ import RegisterReservaModal from '@/app/modals/registerReservas';
 import reservasHeader from '../../../../public/assets/reservasHeader.png'
 import EditReservaModal from '@/app/modals/editarReserva';
 import CheckButton from '../components/CheckButton';
+import VisualizarReservaModal from '@/app/modals/visualizarReserva';
 
 
 export default function Reserva() {
@@ -62,83 +63,85 @@ export default function Reserva() {
 
     return (
         <>
-            <SideBarFuncionario>
-                <div className="w-full h-screen bg-[#DCDCDC]">
-                    <header className="flex">
-                        <div className="bg-white p-4 m-0 flex shadow-lg w-full justify-center items-center h-auto">
-                            <img src={reservasHeader.src} alt="reservasHeader" className="w-24" />
+            <div className=''>
+                <SideBarFuncionario>
+                    <div className="w-full h-screen bg-[#DCDCDC]">
+                        <header className="flex">
+                            <div className="bg-white p-4 m-0 flex shadow-lg w-full justify-center items-center h-auto">
+                                <img src={reservasHeader.src} alt="reservasHeader" className="w-24" />
+                            </div>
+                        </header>
+
+
+                        <div className='ml-20 px-10'>
+                            <RegisterReservaModal
+                                quarto={quarto}
+                                clientes={clientes}
+                                funcionario={funcionario}
+
+                            />
+                            <table className=" w-full text-sm text-left text-white">
+                                <thead className="text-xs text-white0 uppercase bg-gray-50 dark:bg-gray-700">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            Cliente hospedado:
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Está no quarto:
+                                        </th>
+                                        <th scope="col" className="px-4 py-3">
+                                            Status:
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                        </th>
+
+                                    </tr>
+                                </thead>
+
+
+                                <tbody className=''>
+                                    {reserva.map((reservas: any, index) => {
+
+                                        return (
+                                            <>
+                                                <tr key={reservas.id} className=' bg-cyan-800 border-b dark:bg-gray-900 dark:text-white'>
+                                                    <td className='py-4 px-6 indent-[20%]'>
+                                                        {reservas.clientes?.nome}
+
+                                                    </td>
+                                                    <td className='py-4 px-6'>
+                                                        {reservas.quartos?.nome}
+                                                    </td>
+
+                                                    <td className={``}>
+                                                        {reservas.status}
+                                                    </td>
+                                                    <td>
+                                                        <CheckButton id={reservas.id} />
+                                                    </td>
+
+                                                    <td className='flex justify-end gap-4 mt-2 mb-2 pb-0.5 mr-3'>
+                                                        <EditReservaModal
+                                                            quarto={quarto}
+                                                            clientes={clientes}
+                                                            funcionario={funcionario}
+                                                            id={reservas.id}
+                                                        />
+                                                        <VisualizarReservaModal reserva={reservas} />
+                                                    </td>
+                                                </tr>
+                                            </>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
                         </div>
-                    </header>
-
-
-                    <div className='ml-20 px-10'>
-                        <RegisterReservaModal
-                            quarto={quarto}
-                            clientes={clientes}
-                            funcionario={funcionario}
-
-                        />
-                        <table className=" w-full text-sm text-left text-white">
-                            <thead className="text-xs text-white0 uppercase bg-gray-50 dark:bg-gray-700">
-                                <tr>
-                                    <th scope="col" className="px-6 py-3">
-                                        Cliente hospedado:
-                                    </th>
-                                    <th scope="col" className="px-6 py-3">
-                                        Está no quarto:
-                                    </th>
-                                    <th scope="col" className="px-4 py-3">
-                                        Status:
-                                    </th>
-                                    <th scope="col" className="px-6 py-3">
-                                    </th>
-                                    <th scope="col" className="px-6 py-3">
-                                    </th>
-
-                                </tr>
-                            </thead>
-
-
-                            <tbody className=''>
-                                {reserva.map((reservas: any, index) => {
-
-                                    return (
-                                        <>
-                                            <tr key={reservas.id} className=' bg-cyan-800 border-b dark:bg-gray-900 dark:text-white'>
-                                                <td className='py-4 px-6 indent-[20%]'>
-                                                    {reservas.clientes?.nome}
-
-                                                </td>
-                                                <td className='py-4 px-6'>
-                                                    {reservas.quartos?.nome}
-                                                </td>
-
-                                                <td className={``}>
-                                                    {reservas.status}
-                                                </td>
-<td>
-                                                <CheckButton id={reservas.id} />
-                                              </td>
-
-                                                <td className='flex justify-end gap-4 mt-2 mb-2 pb-0.5 mr-3'>
-                                                    <EditReservaModal
-                                                        quarto={quarto}
-                                                        clientes={clientes}
-                                                        funcionario={funcionario}
-                                                        id={reservas.id}
-                                                    />
-                                                </td>
-
-                                            </tr>
-                                        </>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
                     </div>
-                </div>
 
-            </SideBarFuncionario>
+                </SideBarFuncionario>
+            </div>
         </>
 
     )
--- src/app/modals/visualizarReserva/index.tsx
+++ src/app/modals/visualizarReserva/index.tsx
@@ -5,8 +5,10 @@ import { Button, Checkbox, Label, Modal, TextInput } from 'flowbite-react';
 import Cadastro from '../../../../public/assets/cadastro.png'
 import { useForm, SubmitHandler } from "react-hook-form"
 import api from '@/services/api';
+import { compareAsc, format } from 'date-fns'
 import EditItens from '@/functions/editItens';
 import editar from '../../../../public/assets/editar.png'
+import visualizar from '../../../../public/assets/visao.png'
 import editarItem from '../../../../public/assets/editarItem.png'
 import InputMask from 'react-input-mask'
 
@@ -17,7 +19,15 @@ type Inputs = {
     id: number
 }
 
-export default function EditReservaModal({ getItem, index, data, item }: any) {
+export default function VisualizarReservaModal({ reserva }: any) {
+    const quarto = reserva.quartos?.nome;
+    const cliente = reserva.clientes.nome;
+    const dt_inicial = reserva.dt_inicial;
+    const dt_final = reserva.dt_final;
+    const checkin = reserva.check_in;
+    const status = reserva.status;
+    const checkout = reserva.check_out;
+    const funcionario = reserva.users.name;
     const [openModal, setOpenModal] = useState<string | undefined>();
     const props = { openModal, setOpenModal };
     const {
@@ -26,68 +36,70 @@ export default function EditReservaModal({ getItem, index, data, item }: any) {
         reset
     } = useForm<Inputs>()
     const onSubmit: SubmitHandler<Inputs> = (data) => {
-        EditItens({ data })
-            .then((response) => {
-                // window.alert('Item cadastrado com sucesso')
-            }).catch((err) => {
-                console.log(err)
-                console.error(err)
-            });
-        getItem()
         setOpenModal(false as any)
-        reset();
     }
-
-    const NumberInput = () => {
-        const [value, setValue] = useState(0);
-
-        const handleChange = (event: any) => {
-            const inputValue = parseInt(event.target.value, 10);
-
-            if (!isNaN(inputValue) && inputValue >= 0) {
-                setValue(inputValue);
-            }
-        };
-    }
-
-
-    const CurrencyInput = () => {
-        const [inputValue, setInputValue] = useState('');
-
-        const handleInputChange = (event: any) => {
-            const { value } = event.target;
-
-            const validInput = /^\d{1,3}(?:\.\d{3})*(?:,\d{2})?$/.test(value);
-
-            if (validInput) {
-                setInputValue(value);
-            }
-
-        }
-    }
-
 
     return (
         <>
             <div className=''>
                 <Button className="w-[72px]" onClick={() => props.setOpenModal('form-elements')}>
-                    <img src={editar.src} alt="editar" className="border-transparent" />
+                    <img src={visualizar.src} alt="editar" className="border-transparent" />
                 </Button>
-                <Modal show={props.openModal === 'form-elements'} size="md" popup onClose={() => props.setOpenModal(undefined)}>
-                    <Modal.Header />
+                <Modal show={props.openModal === 'form-elements'} size="xl"
+                    className='sm:h-full backdrop-blur-sm py-36'
+                    popup onClose={() => { props.setOpenModal(undefined), console.log(reserva) }}>
+                    <Modal.Header className={'text-white'} />
                     <Modal.Body>
-                        <div className="space-y-6">
+                        <div className="space-y-6 py-10">
                             <div className="flex items-center hover:invert justify-center">
                                 <img src={editarItem.src}
                                     alt="editar-item"
                                     className="w-44 invert"
                                 />
                             </div>
-                            <div>
+                            <div className='grid grid-cols-2 text-white text-md px-4'>
+                                <div className='flex  justify-start gap-4 mt-4'>
+                                    <label className=''>Cliente:</label>
+                                    <h5>{cliente}</h5>
+                                </div>
+
+                                <div className='flex justify-around gap-4 mt-4'>
+                                    <label className=''>Está no quarto:</label>
+                                    <h5>{quarto}</h5>
+                                </div>
+
+                                <div className='flex   justify-start gap-4 mt-4'>
+                                    <label className=''>Inicio:</label>
+                                    <h5>{format(new Date(dt_inicial), 'dd/MM/yyyy')}</h5>
+                                </div>
+
+                                <div className='flex justify-around gap-4 mt-4'>
+                                    <label className=''>Data Final:</label>
+                                    <h5>{format(new Date(dt_final), 'dd/MM/yyyy')}</h5>
+                                </div>
+
+                                <div className='flex   justify-start gap-4 mt-4'>
+                                    <label className=''>Check in:</label>
+                                    <h5>{format(new Date(checkin), 'dd/MM/yyyy')}</h5>
+                                </div>
+
+                                <div className='flex justify-around gap-4 mt-4'>
+                                    <label className=''>Check out:</label>
+                                    <h5>{checkout ? format(new Date(checkin), 'dd/MM/yyyy') : 'Pendente'}</h5>
+                                </div>
+
+                                <div className='flex   justify-start gap-4 mt-4'>
+                                    <label className=''>Cadastrado por:</label>
+                                    <h5>{funcionario}</h5>
+                                </div>
+
+                                <div className='flex justify-around gap-4 mt-4'>
+                                    <label className=''>Status:</label>
+                                    <h5>{status}</h5>
+                                </div>
+
 
                             </div>
-
-
                         </div>
                     </Modal.Body>
                 </Modal>
